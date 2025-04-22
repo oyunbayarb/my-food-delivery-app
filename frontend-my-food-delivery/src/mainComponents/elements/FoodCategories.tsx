5e91bb2835d0726e913b4b14c3be822786195e25
24LP0262 24LP0262 <24LP0262@Setups-iMac.local>
2025-04-22 12:25:33 +0800

    food.cat hiiv

--- frontend-my-food-delivery/src/mainComponents/elements/FoodCategories.tsx
+++ frontend-my-food-delivery/src/mainComponents/elements/FoodCategories.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Carousel,
   CarouselContent,
@@ -5,47 +7,45 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import {useState, useEffect} from "react";
+import {axiosInstance} from "@/lib/utils";
 
+type foodCategoryDataType = {
+  _id: string;
+  name: string;
+};
 export const FoodCategoriesList = () => {
-  const foodCategoriesListData = [
-    "Appetizers",
-    "Salads",
-    "Pizzas",
-    "Lunch favorites",
-    "Main dishes",
-    "Fish & Sea foods",
-    "Side dish",
-    "Brunch",
-    "Desserts",
-    "Beverages",
-    "Appetizers",
-    "Salads",
-    "Pizzas",
-    "Lunch favorites",
-    "Main dishes",
-    "Fish & Sea foods",
-    "Side dish",
-    "Brunch",
-    "Desserts",
-    "Beverages",
-  ];
+  const [foodCategoryData, setFoodCategoryData] = useState<
+    foodCategoryDataType[]
+  >([]);
+
+  const fetchfoodCategoryData = async () => {
+    const res = await axiosInstance.get(`/foodCategory`);
+    setFoodCategoryData(res.data.foodCategory);
+  };
+
+  useEffect(() => {
+    fetchfoodCategoryData();
+  }, []);
+
   return (
     <div className="flex-col">
       <p className="text-white font-semibold text-[30px]">Categories</p>
-
       <Carousel className="flex w-full">
-        <CarouselContent className="w-fit gap-1 flex p-4 overflow-scroll">
-          {foodCategoriesListData.map((value, index) => {
+        <CarouselContent className="w-fit flex p-4 overflow-scroll">
+          {foodCategoryData?.map((element, index) => {
             return (
-              <button
-                key={index}
-                className="bg-white rounded-full min-w-[96px]"
-              >
-                {value}
-              </button>
+              <div>
+                <CarouselItem key={index}>
+                  <button className="bg-white rounded-full min-w-[116px] flex h-[46px] justify-center items-center">
+                    {element.name}
+                  </button>
+                </CarouselItem>
+              </div>
             );
           })}
         </CarouselContent>
+
         <CarouselPrevious />
         <CarouselNext />
       </Carousel>
